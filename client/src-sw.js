79a162707beea6ce1fb53efb5e5436b35401f98a
mkdemon5 <alvaradomike5@gmail.com>
2023-01-17 21:11:37 -0600

    pushing updates

--- client/src-sw.js
+++ client/src-sw.js
@@ -8,23 +8,39 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
+	cacheName: 'page-cache',
+	plugins: [
+		new CacheableResponsePlugin({
+			statuses: [0, 200],
+		}),
+		new ExpirationPlugin({
+			maxAgeSeconds: 30 * 24 * 60 * 60,
+		}),
+	],
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+	urls: ['/index.html', '/'],
+	strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+	({ request }) => request.destination === 'images',
+
+	// sets cache first strategy for images
+	new CacheFirst({
+		cacheName: 'image-cache',
+		plugins: [
+			// plugin added for cacheable responses
+			new CacheableResponsePlugin({
+				statuses: [0, 200],
+			}),
+			new ExpirationPlugin({
+				// sets expiration for 30 days hence
+				maxAgeSeconds: 30 * 24 * 60 * 60,
+			}),
+		],
+	})
+);
